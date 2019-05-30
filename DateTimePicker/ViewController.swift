20670557371c370ab1675c4cf25939a1e4c9974b
YonThiriAung <yonthiriaung041998@gmail.com>
2019-05-30 21:32:47 +0630

    DateTimePickerTest

--- DateTimePicker/ViewController.swift
+++ DateTimePicker/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func datePickerChanged(_ sender: Any) {
+        
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        let strDate = dateFormatter.string(from: datePicker.date)
+        Label.text = strDate
+    }
+    
 }
 
